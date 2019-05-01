fe3fc23b99a75e78e1f6439a3aa3d80b4d05c58d
Robert Wiggins <wigginsro11@gmail.com>
2019-05-01 12:42:02 -0400

    first drills2 exercise

--- createMyObject.js
+++ createMyObject.js
@@ -1,5 +1,4 @@
 'use strict';
-
 function createMyObject() {
   let obj = {
     foo: 'bar',
@@ -11,15 +10,13 @@ function createMyObject() {
   };
   return obj;
 }
- 
-/* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
-     Nothing to see here!
-     
-  */
-  
+/* From here down, you are not expected to
+   understand.... for now :)
+   
+   
+   Nothing to see here!
+   
+*/
 (function testCreateMyObject() {
   var obj = createMyObject();
   if (typeof obj !== 'object') {
@@ -27,43 +24,34 @@ function createMyObject() {
     return false;
   }
   var expectedKeys = ['foo', 'answerToUniverse', 'olly olly', 'sayHello'];
-  expectedKeys.forEach(function(key) {
+  expectedKeys.forEach(function (key) {
     if (!(key in obj)) {
       console.error('ERROR: Missing a key for ' + key);
       return false;
     }
   });
   if (obj.foo !== 'bar') {
-    console.error('ERROR: Value for `foo` should be \'bar\' but was ' + obj.foo);
+    console.error("ERROR: Value for `foo` should be 'bar' but was " + obj.foo);
     return false;
   }
   if (obj.answerToUniverse !== 42) {
-    console.error(
-      'ERROR: Value for `answerToUniverse` should be 42 but was ' +
-          obj.answerToUniverse
-    );
+    console.error('ERROR: Value for `answerToUniverse` should be 42 but was ' +
+      obj.answerToUniverse);
     return false;
   }
   if (obj['olly olly'] !== 'oxen free') {
-    console.error(
-      'ERROR: Value for `\'olly olly\'` should be \'oxen free\' but was ' +
-          obj['olly olly']
-    );
+    console.error("ERROR: Value for `'olly olly'` should be 'oxen free' but was " +
+      obj['olly olly']);
     return false;
   }
   if (!(typeof obj.sayHello === 'function' && obj.sayHello() === 'hello')) {
-    console.error(
-      'ERROR: Value for `sayHello` must be a function that returns the string \'hello\''
-    );
+    console.error("ERROR: Value for `sayHello` must be a function that returns the string 'hello'");
     return false;
   }
   console.log('SUCCESS: Your function works!');
 })();
-
 // console.log(createMyObject());
-
 //   ---------------
-
 function updateObject(obj) {
   obj.foo = 'foo';
   obj.bar = 'bar';
@@ -71,32 +59,30 @@ function updateObject(obj) {
   obj.bang = 'bang';
   return obj;
 }
-
-/* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
-     Nothing to see here!
-     
-  */
-  
+/* From here down, you are not expected to
+   understand.... for now :)
+   
+   
+   Nothing to see here!
+   
+*/
 (function testUpdateObject() {
   var oldObj = {
     cats: 'cats',
     dogs: 'dogs',
   };
   var newObj = updateObject(oldObj);
-  if (typeof newObj !== 'object') { 
+  if (typeof newObj !== 'object') {
     console.error('ERROR: `createMyObject` must return an object');
     return false;
   }
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function (key) {
     if (!(key in newObj)) {
       console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
       return false;
     }
   });
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function (key) {
     if (newObj[key] !== key) {
       console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
       return false;
@@ -107,14 +93,10 @@ function updateObject(obj) {
     return false;
   }
   console.log('SUCCESS: `updateObject` works correctly!');
-    
 })();
-
 // let obj = createMyObject();
 // console.log(updateObject(obj));
-
 //   ---------------
-
 function personMaker() {
   var person = {
     firstName: 'Paul',
@@ -127,15 +109,13 @@ function personMaker() {
   };
   return person;
 }
-  
-/* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
-     Nothing to see here!
-     
-  */
-  
+/* From here down, you are not expected to
+   understand.... for now :)
+   
+   
+   Nothing to see here!
+   
+*/
 (function testPersonMaker() {
   var person = personMaker();
   if (typeof person !== 'object') {
@@ -147,51 +127,41 @@ function personMaker() {
     return false;
   }
   if (person.fullName() !== 'Paul Jones') {
-    console.error(
-      'ERROR: The value for `fullName` should be "Paul Jones" but was ' +
-          person.fullName()
-    );
+    console.error('ERROR: The value for `fullName` should be "Paul Jones" but was ' +
+      person.fullName());
     return false;
   }
   person.firstName = 'Lisa';
   person.lastName = 'Simpson';
   if (person.fullName() !== 'Lisa Simpson') {
-    console.error(
-      '`personMaker` is not using self reference correctly. ' +
-          'When firstName set to "Lisa" and lastName set to "Simpson", ' +
-          'should return "Lisa Simpson" but returned ' +
-          person.fullName()
-    );
+    console.error('`personMaker` is not using self reference correctly. ' +
+      'When firstName set to "Lisa" and lastName set to "Simpson", ' +
+      'should return "Lisa Simpson" but returned ' +
+      person.fullName());
   }
   console.log('SUCCESS: `updateObject` works correctly!');
 })();
-  
 // let person = personMaker();
 // console.log(person.fullName());
-
 //   ---------------
-
 const sampleObj = {
   foo: 'foo',
   bar: 'bar',
   bizz: 'bizz',
   bang: 'bang',
 };
-  
 function keyDeleter(obj) {
   delete obj.foo;
   delete obj.bar;
   return obj;
 }
-  
-/* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
-     Nothing to see here!
-     
-  */
-  
+/* From here down, you are not expected to
+   understand.... for now :)
+   
+   
+   Nothing to see here!
+   
+*/
 (function testKeyDeleter() {
   var obj = keyDeleter({
     foo: 'foo',
@@ -199,18 +169,17 @@ function keyDeleter(obj) {
     bizz: 'bizz',
     bang: 'bang',
   });
-  
   if (typeof obj !== 'object') {
     console.error('ERROR: `keyDeleter` must be return an object');
     return false;
   }
-  ['foo', 'bar'].forEach(function(key) {
+  ['foo', 'bar'].forEach(function (key) {
     if (key in obj) {
       console.error('`keyDeleter` did not delete the key for ' + key);
       return false;
     }
   });
-  ['bizz', 'bang'].forEach(function(key) {
+  ['bizz', 'bang'].forEach(function (key) {
     if (!(key in obj && obj[key] === key)) {
       console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
       return false;
@@ -218,6 +187,4 @@ function keyDeleter(obj) {
   });
   console.log('SUCCESS: `keyDeleter` works correctly!');
 })();
-  
 console.log(keyDeleter(sampleObj));
-
---
+++ obj-drills-2.js
@@ -0,0 +1,172 @@
+'use strict';
+
+// EXERCISE 1
+
+function makeStudentsReport(data) {
+  // your code here
+  
+  
+  let arr = [];
+
+  for (let i = 0; i < data.length; i++) {
+    arr.push(`[${data[i].name}]: [${data[i].grade}]`);
+  }
+
+  return arr;
+
+}
+
+const testData = [
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
+
+// console.log(makeStudentsReport(testData));
+
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testIt() {
+  const testData = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
+  ];
+
+  const expectations = ['Jane Doe: A', 'John Dough: B', 'Jill Do: A'];
+
+  const results = makeStudentsReport(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `makeStudentsReport` must return an array');
+    return;
+  }
+  if (results.length !== testData.length) {
+    console.error(
+      'FAILURE: test data had length of ' +
+        testData.length +
+        ' but `makeStudentsReport` returned array of length ' +
+        results.length
+    );
+    return;
+  }
+  for (let i = 0; i < expectations.length; i++) {
+    let expect = expectations[i];
+    if (
+      !results.find(function(item) {
+        return item === expect;
+      })
+    ) {
+      console.error(
+        'FAILURE: `makeStudentsReport` is not ' + 'producing expected strings'
+      );
+      return;
+    }
+  }
+  console.log('SUCCESS: `makeStudentsReport` is working');
+}
+
+testIt();
+
+
+// -----------------------------------------------------------------
+
+
+// EXERCISE 2 
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  // your code here
+}
+
+/* From here down, you are not expected to understand.... for now :)
+  Nothing to see here!
+
+*/
+
+// tests
+
+function testIt() {
+  var testData = [
+    {
+      name: 'Burt',
+      status: 'Playing hooky',
+      course: 'Biology',
+    },
+    {
+      name: 'Melanie',
+      status: 'Sick',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Leonard',
+      status: 'AWOL',
+      course: 'Computer science',
+    },
+  ];
+
+  var results = enrollInSummerSchool(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `enrollSummerSchool` must return an array');
+    return;
+  }
+  var result = testData.every(function(student) {
+    var match = results.find(function(_student) {
+      return (
+        _student.name === student.name &&
+        _student.course === student.course &&
+        _student.status.toLowerCase() === 'in summer school'
+      );
+    });
+    return match !== undefined;
+  });
+  if (!result) {
+    console.error(
+      'FAILURE: `enrollSummerSchool` should return ' +
+        'original key/value pairs for each student, and ' +
+        'update `status` to "In Summer school": ' +
+        JSON.stringify(results)
+    );
+  } else {
+    console.info('SUCCESS: `enrollSummerSchool` is working');
+  }
+}
+
+testIt();
+
+
+
+
+// ---------------------------------------------------------------
+
+// EXERCISE 3
+
+
+
